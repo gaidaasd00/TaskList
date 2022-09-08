87af97a0ffeeb8439c516c43acfa9826ab84ce0e
gaidaasd00 <swiftgaida@list.ru>
2022-09-08 18:26:13 +0300

    1. new branch alertController 2 . added save in coreData

--- TaskList/TaskListViewController.swift
+++ TaskList/TaskListViewController.swift
@@ -8,11 +8,16 @@
 import UIKit
 
 class TaskListViewController: UITableViewController {
+    private let viewContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    private let cellId = "cell"
+    private var taskList: [Task] = []
 
     override func viewDidLoad() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
         super.viewDidLoad()
         view.backgroundColor = .white
         setupNavigationBar()
+        fetchData()
     }
     // работа над внешним видом navigationBar
     private func setupNavigationBar() {
@@ -40,8 +45,64 @@ class TaskListViewController: UITableViewController {
         navigationController?.navigationBar.tintColor = .white
     }
     @objc private func addNewTask() {
-        let newTaksViewController = TaskViewController()
-        present(newTaksViewController, animated: true)
+        alertController(with: "New Task", andMessage: "What do you wont to do?")
+    }
+    private func fetchData() {
+        let fetchRequest = Task.fetchRequest()
+        
+        do {
+            taskList = try viewContext.fetch(fetchRequest)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    private func alertController(with title: String, andMessage message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) {[unowned self] _ in
+            guard let task = alert.textFields?.first?.text, !task.isEmpty else { return }
+            save(task)
+        }
+        let cancleAction = UIAlertAction(title: "Cancle", style:.destructive)
+        alert.addAction(saveAction)
+        alert.addAction(cancleAction)
+        alert.addTextField { textField in
+            textField.placeholder = "New Task"
+        }
+        present(alert, animated: true)
+    }
+    private func save(_ taskName: String) {
+        //создание экземпляра модели
+        let task = Task(context: viewContext)
+        //изменение модели
+        task.title = taskName
+        taskList.append(task)
+        
+        let cellIndex = IndexPath(row: taskList.count - 1, section: 0)
+        tableView.insertRows(at: [cellIndex], with: .automatic)
+        
+        // сохранение в постоянное хранилеще
+        if viewContext.hasChanges {
+            do {
+                try viewContext.save()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
     }
 }
+
+extension TaskListViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        taskList.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let task = taskList[indexPath.row]
+        var content = cell.defaultContentConfiguration()
+        content.text = task.title
+        cell.contentConfiguration = content
+        return cell
+    }
+}
+
 
